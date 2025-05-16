563e3aec978be5bab5ec441ee467fe38a7f84e28
Krusz Beton <199311169+Krusz-Beton@users.noreply.github.com>
2025-05-16 17:00:49 +0200

    chyba ok

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,18 +7,16 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { provideHttpClient } from '@angular/common/http';
-//import { ServiceWorkerModule } from '@angular/service-worker';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, 
-//     ServiceWorkerModule.register('ngsw-worker.js', {
-//   enabled: !isDevMode(),
-//   // Register the ServiceWorker as soon as the application is stable
-//   // or after 30 seconds (whichever comes first).
-//   registrationStrategy: 'registerWhenStable:30000'
-// })
-],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, ServiceWorkerModule.register('ngsw-worker.js', {
+  enabled: !isDevMode(),
+  // Register the ServiceWorker as soon as the application is stable
+  // or after 30 seconds (whichever comes first).
+  registrationStrategy: 'registerWhenStable:30000'
+})],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, provideHttpClient(), provideClientHydration(withEventReplay()),],
   bootstrap: [AppComponent],
 })
