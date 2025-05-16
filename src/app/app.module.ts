26ff2189b6af61617ab97f4be0ae6d1df0dcf021
Krusz Beton <199311169+Krusz-Beton@users.noreply.github.com>
2025-05-16 19:07:36 +0200

    2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { NgModule, isDevMode } from '@angular/core';
-import { BrowserModule, withEventReplay} from '@angular/platform-browser';
+import { BrowserModule, provideClientHydration, withEventReplay} from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
@@ -19,7 +19,7 @@ import { ServiceWorkerModule } from '@angular/service-worker';
   registrationStrategy: 'registerWhenStable:30000'
 })
 ],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, provideHttpClient() 
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, provideHttpClient(),
     provideClientHydration(withEventReplay()),
   ],
   bootstrap: [AppComponent],
