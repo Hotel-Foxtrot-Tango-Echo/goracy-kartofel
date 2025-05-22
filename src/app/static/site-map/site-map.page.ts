46781e3e737061965c54b8aa1fb81df270bc80c2
Krusz Beton <199311169+Krusz-Beton@users.noreply.github.com>
2025-05-22 15:30:24 +0200

    update test 2

--- src/app/static/site-map/site-map.page.ts
+++ src/app/static/site-map/site-map.page.ts
@@ -1,8 +1,10 @@
-import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnDestroy, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
-import { SwUpdate, VersionReadyEvent } from '@angular/service-worker';
+import { SwUpdate, VersionEvent, VersionReadyEvent } from '@angular/service-worker';
 import { filter } from 'rxjs';
 import { TITLE_SEP, TITLE_BASE } from 'src/app/shared/const';
+import { FilterDataRptr } from 'src/app/shared/services/filter.service';
+import { SubSink } from 'subsink';
 
 @Component({
   selector: 'app-site-map',
@@ -11,30 +13,38 @@ import { TITLE_SEP, TITLE_BASE } from 'src/app/shared/const';
   standalone: false,
   changeDetection: ChangeDetectionStrategy.OnPush    
 })
-export class SiteMapPage implements OnInit {
+export class SiteMapPage implements OnInit,OnDestroy {
 
   siteName = 'Mapa strony'
   isNewVesrion = false;
+  private subSink = new SubSink();
+  
 
   constructor(
         private title: Title,
         private swUpdate: SwUpdate,
         private changeDetectorRef: ChangeDetectorRef,
-  ) {
+  ) { }
 
-    async () => {
-      try {
-        const updateFound = await this.swUpdate.checkForUpdate();
-        console.log(updateFound ? 'A new version is available.' : 'Already on the latest version.');
-        setTimeout(() => {
-          this.isNewVesrion = true;
-          this.changeDetectorRef.markForCheck();
-        },0)        
-      } catch (err) {
-        console.error('Failed to check for updates:', err);
-      }
-    }
+  ngOnInit(): void {
+    this.subSink.sink = this.swUpdate.versionUpdates.subscribe({
+      next:(ev: VersionEvent) => {
+          console.log(ev)    
+          setTimeout(() => {
+            this.isNewVesrion = true
+            this.changeDetectorRef.markForCheck();    
+          },5000)     
+      },
+      error: (e) => {
+        console.log('error',e)
+      },
+    })  
+    this.title.setTitle(this.siteName + TITLE_SEP + TITLE_BASE)
 
+  }  
+
+  ngOnDestroy():void {
+    this.subSink.unsubscribe();
   }
 
   downoladLast() {
@@ -48,10 +58,6 @@ export class SiteMapPage implements OnInit {
         document.location.reload();
       // }
     });
-  }
-
-  ngOnInit() {
-    this.title.setTitle(this.siteName + TITLE_SEP + TITLE_BASE)
   }
 
 }
