f1e9460c80f0b9c2285084a7df5e1c6537602a8b
Krusz Beton <199311169+Krusz-Beton@users.noreply.github.com>
2025-11-22 00:05:55 +0100

    test4

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -18,17 +18,16 @@ const routes: Routes = [
     path: 'export',
     loadChildren: () => import('./export/export.module').then( m => m.ExportPageModule)
   },  
-    {path: '**', redirectTo: 'przemienniki'},
-
-  {
-    path: '',
-    redirectTo: 'przemienniki',
-    pathMatch: 'full'
-  },
   // {
   //   path: '',
-  //   loadChildren: () => import('./static/start/start.module').then( m => m.StartPageModule)
+  //   redirectTo: 'przemienniki',
+  //   pathMatch: 'full'
   // },
+  {
+    path: '',
+    loadChildren: () => import('./static/start/start.module').then( m => m.StartPageModule)
+  },
+  {path: '**', redirectTo: 'przemienniki'},
 
   // {
   //   path: '',
