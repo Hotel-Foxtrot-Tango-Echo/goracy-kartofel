c2886ea10879d3099e95a18b7aa57aafd0fcd0d2
Krusz Beton <199311169+Hotel-Foxtrot-Tango-Echo@users.noreply.github.com>
2026-08-24 22:05:22 +0200

    remove pirvacy policy page

--- src/app/static/static.module.ts
+++ src/app/static/static.module.ts
@@ -7,10 +7,6 @@ const routes: Routes = [
     loadChildren: () => import('./news/news.module').then( m => m.NewsPageModule)
   },
   {
-    path: 'privacy-policy',
-    loadChildren: () => import('./privacy-policy/privacy-policy.module').then( m => m.PrivacyPolicyPageModule)
-  },
-  {
     path: 'site-map',
     loadChildren: () => import('./site-map/site-map.module').then( m => m.SiteMapPageModule)
   },  
